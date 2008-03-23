e90fa294982e43e10e4547d217c5d10505637119
Francesc Esplugas <fesplugas@intraducibles.net>
2008-03-23 20:44:18 +0100

    Added tests

--- test/functional/page_controller_test.rb
+++ test/functional/page_controller_test.rb
@@ -1,8 +1,21 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class PageControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  fixtures :pages
+
+  def test_should_show_published_page
+    page = pages(:about)
+    get :show, { :anything => [page.slug] }
+    assert_response :success
+    assert_template 'show'
   end
+
+  def test_should_not_show_unpublished_page
+    page = pages(:unpublished)
+    assert_raise(ActiveRecord::RecordNotFound){
+      get :show, { :anything => [page.slug] }
+    }
+  end
+
 end
